c7fbfb54a86a9662e39074effd0994cc548b9d78
Randy Concepcion <randy.concepcion@policygenius.com>
2020-12-03 01:56:18 -0500

    Day 03: Part 2 - All the slopes

--- 2020/day03/main.py
+++ 2020/day03/main.py
@@ -1,7 +1,3 @@
-SLOPE_X = 3
-SLOPE_Y = 1
-
-
 def get_map_section_from_file(filename):
     map_section = []
     with open(filename) as file:
@@ -10,17 +6,17 @@ def get_map_section_from_file(filename):
     return map_section
 
 
-def get_number_of_trees():
+def get_number_of_trees(slope_x, slope_y):
     MAP_SECTION = get_map_section_from_file("input.txt")
     MAP_WIDTH = len(MAP_SECTION[0])
     MAP_HEIGHT = len(MAP_SECTION)
 
     x = 0
     trees_count = 0
-    for y in range(0, MAP_HEIGHT - 1):
+    for y in range(0, MAP_HEIGHT - 1, slope_y):
         # Navigate the map section
-        y += SLOPE_Y
-        x += SLOPE_X
+        y += slope_y
+        x += slope_x
 
         # Did x exceed MAP_WIDTH?
         if x >= MAP_WIDTH:
@@ -34,8 +30,19 @@ def get_number_of_trees():
     return trees_count
 
 def main():
-    num_trees = get_number_of_trees()
-    print(f'Number of trees: {num_trees}')
+    num_trees1 = get_number_of_trees(1, 1)
+    num_trees2 = get_number_of_trees(3, 1)
+    num_trees3 = get_number_of_trees(5, 1)
+    num_trees4 = get_number_of_trees(7, 1)
+    num_trees5 = get_number_of_trees(1, 2)
+    print(f'Number of trees1: {num_trees1}')
+    print(f'Number of trees2: {num_trees2}')
+    print(f'Number of trees3: {num_trees3}')
+    print(f'Number of trees4: {num_trees4}')
+    print(f'Number of trees5: {num_trees5}')
+
+    product = num_trees1 * num_trees2 * num_trees3 * num_trees4 * num_trees5
+    print(f'Product of all trees: {product}')
 
 if __name__ == "__main__":
     main()
