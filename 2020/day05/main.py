edca94d7f5aa37b6264b6124a6006a649b9c3a6c
Randy Concepcion <randy.concepcion@policygenius.com>
2020-12-05 01:14:18 -0500

    Day 05: Part 2

--- 2020/day05/main.py
+++ 2020/day05/main.py
@@ -43,9 +43,36 @@ def get_highest_seat_id():
     return highest_seat_id
 
 
+def get_my_seat():
+    seat_list = (get_values_from_file("input.txt"))
+    valid_seat_list = []
+
+    for seat in seat_list:
+        row_num = get_row(seat)
+
+        if row_num == 0 or row_num == 127:
+            continue
+
+        col_num = get_column(seat)
+
+        seat_id = row_num * 8 + col_num
+        valid_seat_list.append(seat_id)
+
+    valid_seat_list.sort()
+
+    for i in range(0, len(valid_seat_list) - 1):
+        if (valid_seat_list[i] + 1) != valid_seat_list[i+1]:
+            return valid_seat_list[i] + 1
+
+    return 0
+
+
 def main():
     highest_seat_id = get_highest_seat_id()
     print(f'Highest seat ID: {highest_seat_id}')
+
+    my_seat_id = get_my_seat()
+    print(f'My seat ID: {my_seat_id}')
 
 
 if __name__ == "__main__":
